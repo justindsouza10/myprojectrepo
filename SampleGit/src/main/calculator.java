a7ba7ec2c3495cf11827274a22b2845cbf9cc69b
justindsouza10 <81158397+justindsouza10@users.noreply.github.com>
2021-03-22 18:31:27 +0530

    added diff function

--- SampleGit/src/main/calculator.java
+++ SampleGit/src/main/calculator.java
@@ -8,5 +8,8 @@ public class calculator {
 	public static void add(int num1,int num2){
 	      System.out.println(num1+num2);
 	  }
+	public static void diff(int num1,int num2){
+	      System.out.println(num1-num2);
+	  }
 
 }
