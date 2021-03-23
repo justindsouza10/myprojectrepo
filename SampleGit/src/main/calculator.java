5dea378c7028386141e36b02c5be5b0980a7f521
justindsouza10 <justindsouza.14@gmail.com>
2021-03-23 10:53:43 +0530

    added cube

--- SampleGit/src/main/calculator.java
+++ SampleGit/src/main/calculator.java
@@ -28,6 +28,9 @@ public class calculator {
 	public static void sqr(int num1){
 	      System.out.println(num1*num1);
 	  }
+	public static void cube(int num1){
+	      System.out.println(num1*num1*num1);
+	  }
 
 
 }
